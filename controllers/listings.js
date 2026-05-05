1224076d3f8211b08f6a380ba8eb681481a28fe4
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-05-05 11:26:01 +0530

    Remove unnecessary whitespace in listings.js

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,9 +1,7 @@
  const Listing = require("../models/listing");
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-
 const mapToken = process.env.MAP_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
-
 module.exports.index = async (req, res) => {
   const { search, filter } = req.query;
 
